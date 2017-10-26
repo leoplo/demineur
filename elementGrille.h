3bf96f8d4d9967f84f19af895fdace036a28b41f
Gauthiervangre <vangrevelynghe.gauthier@hotmail.com>
2017-10-26 15:19:41 +0200

    MAJ structure + test main

--- elementGrille.h
+++ elementGrille.h
@@ -1,10 +1,11 @@
 #ifndef ELEMENTGRILLE_H_INCLUDED
 #define ELEMENTGRILLE_H_INCLUDED
 
-typedef struct ElementGrille {
+struct elementGrille {
     int presenceMine; // 1 si true et 0 si false
     int presenceDrapeau; // 1 si true et 0 si false
-    int caseRevelee // 1 si true et 0 si false
-} ElementGrille;
+    int caseRevelee; // 1 si true et 0 si false
+};
 
+typedef struct elementGrille ElementGrille;
 #endif // ELEMENTGRILLE_H_INCLUDED
--- main.c
+++ main.c
@@ -1,24 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "menu.h"
-#include "grille.h"
-#include "partie.h"
-
-
-
-
-
+#include "menu.c"
+#include "grille.c"
+#include "mines.c"
+#include "partie.c"
 
 int main()
 {
-    int i, taille=10;
-    char** partie;
-
-    i=menu();
+int i, taille=10;
+char** partie;
+i=menu();
     if(i==1)
     {
         partie = nouvellePartie(taille);
+        ElementGrille** grille = remplirGrille();
         afficherGrille(taille, partie);
-
+        int** mines = genererMines(10);
+        afficherMines(mines, 10);
+        remplirGrilleAvecMine(grille, mines,10);
+        lireGrille(grille, 10);
         for(i=0;i<taille;i++)
         {
             free(partie[i]);
